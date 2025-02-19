a5bc5d93d6c4e8ec42bd629410eabf33df4b4c81
Sumit Malviya <136370734+Mjlonir29@users.noreply.github.com>
2025-02-19 21:39:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ async function fetchWeather() {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
     try {
+        
         const response = await fetch(url);
         const data = await response.json();
         document.getElementById("weather").innerHTML = `
